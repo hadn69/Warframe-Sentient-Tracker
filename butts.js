43b45fe6d6049b1da0d5410d69ddcc5bd5a1ef80
Hayden Neumeyer <hadn6969@gmail.com>
2019-12-25 22:25:50 -0800

    used supply of elbow grease

--- butts.js
+++ butts.js
@@ -1,12 +1,18 @@
 var xmlhttp = new XMLHttpRequest();
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes * 60000);
+}
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var obj = JSON.parse(this.responseText);
         if (obj.active == 0) {
+            var date = new Date(obj.expiry);
+            var eta = addMinutes(date, 185);
             document.getElementById("status").innerHTML = "No";
             document.getElementById("status").style.color = "red";
             document.getElementById("mission").innerHTML = "";
-            document.getElementById("expires").innerHTML = "";
+            document.getElementById("expires").innerHTML = "ETA of next encounter: </br>" + eta.toTimeString();
         } else {
             document.getElementById("status").innerHTML = "Yes";
             document.getElementById("status").style.color = "green";
